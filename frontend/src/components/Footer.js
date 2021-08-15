80f751a40ca6ca0eb4dff5eaf5c10958106c2715
Juan Eduardo Castro Trujillo <jc.venepro@gmail.com>
2021-08-14 23:33:00 -0300

    14th commit

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,30 +1,46 @@
 import { NavLink, Link } from "react-router-dom"
+import React, { useState, useEffect } from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faUserCircle } from '@fortawesome/free-solid-svg-icons'
+
 
 const Footer = () => {
-    return(
+
+    const [menuGuy, setMenuGuy] = useState(false)
+
+    const toggleMenuGuy = () => {
+        setMenuGuy(!menuGuy)
+    } 
+
+
+    return (
         <footer className="bg-indigo-500 bg-opacity-90 w-full h-60 md:h-96 flex flex-col justify-between">
             <div className="flex flex-col items-center justify-center w-full gap-8 md:flex md:flex-row md:justify-around md:items-center md:w-5/6 md:h3/5 pt-12">
                 <div className=" justify-center items-center px-8 gap-3 text-5xl font-semibold tracking-wider hidden md:block permanentMarkerFont">
                     <img src="/assets/logo_mytinerary.png" alt="logo mytinenrary" className="w-28 h-28 m-1"/>
                     <h1>Mytinerary</h1>
                 </div>
-                <div className="flex flex-row justify-center me:flex md:flex-col md:justify-start gap-6 md:gap-6 text-center">
-                    <div className=" text-2xl md:text-3xl">
-                        <h2>Menu</h2>
-                    </div>
-                    <div className="flex flex-row items-end gap-6 md:gap-6 md:flex md:flex-col md:items-start md:gap-6 text-lg md:text-xl">
-                        <NavLink exact to="/" activeclasscame="text-indigo-700" className="hover:text-indigo-700">
-                            <h2>Home</h2>
-                        </NavLink>
-                        <NavLink to="/cities" activeclasscame="text-indigo-700" className="hover:text-indigo-700">
-                            <h2>Cities</h2>
-                        </NavLink>
-                        <Link to="" className="hover:text-indigo-700">
-                            <h2>Log in</h2>
-                        </Link>
-                        <Link to="" className="hover:text-indigo-700">
-                            <h2>Sign up</h2>
-                        </Link>
+
+                <div className="flex flex-row items-end gap-6 md:gap-6 md:flex md:flex-col md:items-start md:gap-6 text-lg md:text-xl bg-blue-300">
+                    <NavLink exact to="/" activeClassName="text-indigo-700" className="hover:text-indigo-700">
+                        <h2>Home</h2>
+                    </NavLink>
+                    <NavLink to="/cities" activeClassName="text-indigo-700" className="hover:text-indigo-700">
+                        <h2>Cities</h2>
+                    </NavLink>
+                    
+                    <div className="w-28 flex flex-col items-end md:items-end md:justify-end bg-pink-300">
+                        {menuGuy && (
+                            <ul className="flex flex-col items-end text-center list-none gap-4 text-xl cursor-pointer py-2.5 px-4 overflow-visible">
+                                <li to="" activeclassname="text-indigo-700" className="hover:text-indigo-700"><p>Log in</p></li>
+                                <li to="" activeclassname="text-indigo-700" className="hover:text-indigo-700"><p>Sign up</p></li>
+                            </ul>
+                        )}
+                        <div className="pt-2 pl-2 m-1 md:pl-0 md:m-0">
+                            <div className="w-14 h-14 md:w-16 md:h-16">
+                                <FontAwesomeIcon icon={faUserCircle} onClick={toggleMenuGuy} size="3x" />
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div>
@@ -50,3 +66,11 @@ const Footer = () => {
 }
 
 export default Footer
+
+
+
+
+                {/* <div className="flex flex-row justify-center me:flex md:flex-col md:justify-start gap-6 md:gap-6 text-center">
+                    <div className=" text-2xl md:text-3xl">
+                        <h2>Menu</h2>
+                    </div> */}
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,10 @@
 import { Link, NavLink } from "react-router-dom"
 import React, { useState, useEffect } from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faUserCircle } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
+    
     const [menuButton, setMenuButton] = useState(false)
     const [menuGuy, setMenuGuy] = useState(false)
     const [screenWidth, setScreenWidth] = useState(window.innerWidth)
@@ -11,11 +14,8 @@ const Header = () => {
     }
 
     const toggleMenuGuy = () => {
-        
         setMenuGuy(!menuGuy)
     }
-    console.log(menuGuy)
-    console.log(menuButton)
 
     useEffect(() => {
         const changeWidth = () => {
@@ -29,10 +29,7 @@ const Header = () => {
         }
     }, [])
 
-
-
     return (
-
         <header className="w-full h-28 md:h-28 sticky top-0">
             <nav className="h-full flex justify-between items-start md:items-center  ">
                 <div className="hidden md:flex md:items-center md:block py-2.5 px-8 gap-3 text-5xl tracking-wider permanentMarkerFont leading-10">
@@ -40,70 +37,42 @@ const Header = () => {
                     <h1 className="">Mytinerary</h1>
                 </div>
                 <div className="w-full h-full flex justify-between md:justify-end items-start md:items-start pt-4 px-8">
-
-                    <div className=" flex flex-col items-start justify-center gap-2 pt-2">
-                        <button className="p-2 text-xl text-black border border-black rounded-md block md:hidden" 
-                        onClick={toggleMenuButton}
-                        >BTN</button>
+                    <div className=" flex flex-col items-start justify-center gap- pt-2">
+                        <div className="w-14 h-14 p-2 text-xl text-black block md:hidden">
+                            <FontAwesomeIcon icon={faBars} onClick={toggleMenuButton} size="2x" />
+                        </div>
                         {(menuButton || screenWidth > 768) && (
                             <ul className=" flex flex-col md:flex md:flex-row text-center list-none gap-4 text-xl cursor-pointer py-2.5 px-2 md:pt-4 overflow-visible text-bolder">
-                                <li to="/" activeclassname="text-indigo-700" className="hover:text-indigo-700"><p>Home</p></li>
-                                <li to="/cities" activeclassname="text-indigo-700" className="hover:text-indigo-700"><p>Cities</p></li>
+                                <li>
+                                    <NavLink exact to="/" activeClassName="text-indigo-700" className="hover:text-indigo-700">
+                                        <p>Home</p>
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/cities" activeClassName="text-indigo-700" className="hover:text-indigo-700">
+                                        <p>Cities</p>
+                                    </NavLink>
+                                </li>
                             </ul>
                         )}
                     </div>
                     <div className="w-28 flex flex-col items-end md:items-end md:justify-end">
-                        <div className="pl-2 m-1 md:pl-0 md:m-0">
-                            <button ><img src="/assets/login-icon-01.png" alt="logo icon" 
-                            onClick={toggleMenuGuy} className="w-14 h-14 md:w-16 md:h-16"/></button>
+                        <div className="pt-2 pl-2 m-1 md:pl-0 md:m-0">
+                            <div className="w-14 h-14 md:w-16 md:h-16">
+                                <FontAwesomeIcon icon={faUserCircle} onClick={toggleMenuGuy} size="4x" />
+                            </div>
                         </div>
                         {menuGuy && (
-                            <ul className="flex flex-col items-end text-center list-none gap-4 text-xl cursor-pointer py-2.5 px-4 overflow-visible bg-indigo-200 bg-opacity-50">
+                            <ul className="flex flex-col items-end text-center list-none gap-4 text-xl cursor-pointer py-2.5 px-4 overflow-visible">
                                 <li to="" activeclassname="text-indigo-700" className="hover:text-indigo-700"><p>Log in</p></li>
                                 <li to="" activeclassname="text-indigo-700" className="hover:text-indigo-700"><p>Sign up</p></li>
                             </ul>
                         )}
                     </div>
                 </div>
-
-                {/* <ul className="list-none flex">
-                    <li className="mr-2 text-2xl text-black cursor-pointer">Home</li>
-                    <li className="mr-2 text-2xl text-black cursor-pointer">Cities</li>
-                    <li className="mr-2 text-2xl text-black cursor-pointer">Other</li>   
-                </ul> */}
             </nav>
         </header>             
     )
 }
 
 export default Header
-
-/* 
-x icon <i class="fas fa-times"></i>
-3 bars icon <i class="fas fa-bars"></i>
- */
-
-
-{/* <header className=" w-full ">
-                    <nav className="flex md:justify-between px-8 gap-4 text-xl "> 
-                <div className="md:flex md:items-center hidden md:block py-2.5 px-8 gap-3 text-6xl tracking-wider permanentMarkerFont leading-10">
-                    <img src="https://i.imgur.com/slgCIZZ.png" alt="logo mytinerary" className="w-24 h-24 m-1"/>
-                    <h1 className="">Mytinerary</h1>
-                </div>
-                <div className="flex justify-between md:justify-end items-center w-full py-3 px-1 md:px-4 gap-4">
-                    <div className="flex gap-4">
-                        <NavLink exact to="/" activeclassname="text-indigo-700" className="hover:text-indigo-700"><p>Home</p></NavLink>
-                        <NavLink to="/cities" activeclassname="text-indigo-700" className="hover:text-indigo-700"><p>Cities</p></NavLink>
-                    </div>
-                    <div className="flex items-center">
-                        <div className="flex gap-3">
-                            <Link to="" activeclassname="text-indigo-700" className="hover:text-indigo-700"><p>Log in</p></Link>
-                            <Link to="" activeclassname="text-indigo-700" className="hover:text-indigo-700"><p>Sign up</p></Link>
-                        </div>
-                        <div className="pl-2 m-1">
-                            <Link to=""><img src="/assets/login-icon-01.png" alt="logo icon" className="w-12 h-12"/></Link>
-                        </div>
-                    </div>
-                </div> 
-            </nav>
-        </header>*/}
