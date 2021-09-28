1873c30aa0f7f8149a903c99886fbaad4b3dbb52
Juan Eduardo Castro Trujillo <jc.venepro@gmail.com>
2021-09-28 19:03:10 -0300

    production frontend

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require("dotenv").config()
 require("./config/database")
 require("./config/passport")
 const router = require("./routes/index")
+const path = require("path")
 
 const app = express()
 
@@ -13,5 +14,9 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/api", router)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"))
+    app.get("*", (req, res) => res.sendFile(path.join(__dirname+"/client/build/index.html")))
+}
 
 app.listen(process.env.PORT || 4000, process.env.HOST || "0.0.0.0", () => console.log("Server listening on port 4000"))
