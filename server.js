8758df738205c749b8f36f87c7f08799e41d4032
Juan Eduardo Castro Trujillo <jc.venepro@gmail.com>
2021-09-28 20:27:40 -0300

    correcion puerto en server.js

--- server.js
+++ server.js
@@ -22,4 +22,4 @@ if (process.env.NODE_ENV === "production") {
     })
 }
 
-app.listen(process.env.PORT || 4000, process.env.HOST || "0.0.0.0", () => console.log("Server listening on port 4000"))
+app.listen(process.env.PORT, process.env.HOST, () => console.log("Server listening"))
