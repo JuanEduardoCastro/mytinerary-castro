fea9955bd636bea4612b07274ebb2ca949293731
Juan Eduardo Castro Trujillo <jc.venepro@gmail.com>
2021-09-28 19:51:38 -0300

    production frontend

--- server.js
+++ server.js
@@ -14,9 +14,12 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/api", router)
+
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"))
-    app.get("*", (req, res) => res.sendFile(path.join(__dirname+"/client/build/index.html")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname+"/client/build/index.html"))
+    })
 }
 
 app.listen(process.env.PORT || 4000, process.env.HOST || "0.0.0.0", () => console.log("Server listening on port 4000"))
